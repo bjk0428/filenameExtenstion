ff7939fca58e2fa0ff4cc31eccdc5743af8e35a3
Jokyoung <bjk0428@naver.com>
2022-10-13 18:49:04 +0900

    init

---
+++ filenameExtenstion/src/main/java/com/filenameExtenstion/extension/FilenameExtenstionApplication.java
@@ -0,0 +1,14 @@
+package com.filenameExtenstion.extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilenameExtenstionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilenameExtenstionApplication.class, args);
+		System.out.println("Test");
+	}
+
+}
